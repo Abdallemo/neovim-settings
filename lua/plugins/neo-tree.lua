eb06183ac851b3b72fbd4d5f70528bed713709e3
EngCamo <learn3038it@gmail.com>
2025-04-02 14:53:04 +0800

    somechnages

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,26 @@
 return {
-
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+		require("neo-tree").setup({
+			close_if_last_window = false,
+			window = {
+				position = "left",
+				width = 30,
+        padding = 0,
+			},
+			filesystem = {
+				follow_current_file = { enabled = true },
+			},
+		})
+
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>")
+		vim.keymap.set("n", "<leader>nb", ":Neotree buffers reveal left toggle<CR>")
+		vim.keymap.set("n", "<leader>ng", ":Neotree git_status reveal float toggle<CR>")
 	end,
 }
